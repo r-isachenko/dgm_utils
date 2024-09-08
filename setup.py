113e3e70df5446d91a1e02f71f5078bc3d678001
Ksenofontov Gregory <49430902+gregkseno@users.noreply.github.com>
2024-09-08 11:11:36 +0300

    Add packages and python version requirement

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from distutils.core import setup
 from setuptools import find_packages
+import sys
+
+
+if sys.version_info < (3, 11):
+    sys.exit('Sorry, Python < 3.11 is not supported')
 
 setup(
     name="dgm_utils",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "matplotlib",
-        "pillow",
-        "tqdm",
-        "torch",
-        "torchvision",
-        "torchaudio",
-        "gdown",
-        "scikit-learn",
+        "numpy==2.1.1",
+        "matplotlib==3.9.2",
+        "pillow==10.4.0",
+        "tqdm==4.66.5",
+        "torch==2.4.1",
+        "torchvision==0.19.1",
+        "torchaudio==2.4.1",
+        "gdown==5.2.0",
+        "scikit-learn==1.5.1"
     ],
 )
